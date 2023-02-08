641749f15ae7d3903a3ee2b7f8392fa305909194
Mehrac Mammadov <mehrajvm@code.edu.az>
2023-02-08 11:38:39 +0400

    chevron is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
 $(document).ready(function () {
   let last;
+  let lastIcon;
   $(".sect").click(function () {
-    if(last){
-      last.slideToggle()
+    if (last) {
+      last.slideUp();
+      lastIcon.classList.toggle("fa-rotate-180");
     }
-    let sect = $(this)
-    let sectParagraph =  sect[0].nextElementSibling
-    sectParagraph.setAttribute("data-id", this.id)
-    last = $(`[data-id = ${this.id}]`).slideToggle()
-    let icon = sect[0].lastElementChild
-    icon.setAttribute("rotated", this.id)
-    let rotated = $(`[rotated = ${this.id}]`)
-    rotated[0].classList.toggle("fa-rotate-180")
+
+    let sect = $(this);
+
+    let sectParagraph = sect[0].nextElementSibling;
+    sectParagraph.setAttribute("data-id", this.id);
+    last = $(`[data-id = ${this.id}]`).slideToggle();
+
+    // last[0].removeAttribute("data-id")
+    // console.log(last[0].getAttribute("data-id"));
+
+    let icon = sect[0].lastElementChild;
+    icon.setAttribute("rotated", this.id);
+    let rotated = $(`[rotated = ${this.id}]`);
+    lastIcon = rotated[0];
+    rotated[0].classList.toggle("fa-rotate-180");
   });
 });
