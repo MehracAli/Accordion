5458cd26ead8e94d9df76c0f87fbbbfcb9bfd6b7
Mehrac Mammadov <mehrajvm@code.edu.az>
2023-02-08 11:48:32 +0400

    all bugs fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,11 @@ $(document).ready(function () {
     let sect = $(this);
 
     let sectParagraph = sect[0].nextElementSibling;
-    sectParagraph.setAttribute("data-id", this.id);
-    last = $(`[data-id = ${this.id}]`).slideToggle();
 
-    // last[0].removeAttribute("data-id")
-    // console.log(last[0].getAttribute("data-id"));
+    if(!sectParagraph.getAttribute("data-id")){
+      sectParagraph.setAttribute("data-id", this.id);
+      last = $(`[data-id = ${this.id}]`).slideToggle();
+    }
 
     let icon = sect[0].lastElementChild;
     icon.setAttribute("rotated", this.id);
