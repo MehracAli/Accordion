d0dac9e757bba084bfd9136539f049bf257dba41
Mehrac Mammadov <mehrajvm@code.edu.az>
2023-02-08 11:54:26 +0400

    chevron is working again

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,12 @@ $(document).ready(function () {
     let sect = $(this);
 
     let sectParagraph = sect[0].nextElementSibling;
+    
+    sectParagraph.setAttribute("data-id", this.id);
+    last = $(`[data-id = ${this.id}]`).slideToggle();
 
-    if(!sectParagraph.getAttribute("data-id")){
-      sectParagraph.setAttribute("data-id", this.id);
-      last = $(`[data-id = ${this.id}]`).slideToggle();
-    }
+    // last[0].removeAttribute("data-id")
+    // console.log(last[0].getAttribute("data-id"));
 
     let icon = sect[0].lastElementChild;
     icon.setAttribute("rotated", this.id);
